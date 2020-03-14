37056ff7aa4f94cb513158ccfafcf849056b06bf
triest <you@example.com>
2020-03-14 14:45:50 +0300

    get page

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,47 @@
 package com.company;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        try (final WebClient webClient = new WebClient()) {
-            final HtmlPage page = webClient.getPage("http://htmlunit.sourceforge.net");
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+        final WebClient webClient = new WebClient();
+        HtmlPage page = null;
+        try {
+            page = webClient.getPage("http://google.com");
         } catch (IOException e) {
-            e.printStackTrace();
+            //     e.printStackTrace();
+            System.out.println("Exaption");
         }
+
+        HtmlInput searchBox = page.getElementByName("q");
+        searchBox.setValueAttribute("Selenium");
+/*
+        HtmlAnchor seleniumButton = page.getAnchorByText("Selenium");
+        try {
+            seleniumButton.click();
+        } catch (IOException e) {
+            System.out.println("Exaption");
+        }
+        */
+
+        //   List<HtmlDivision> date = page.getByXPath("//div[@class='question question_short']");
+        //   System.out.println(date.toString());
+
+
+        //    HtmlSubmitInput googleSearchSubmitButton = page.getElementByName("btnK"); // sometimes it's "btnK"
+
+        //  page=googleSearchSubmitButton.click();
+
+        // HtmlDivision resultStatsDiv = page.getFirstByXPath("//div[@id='resultStats']");
+
+        //    System.out.println(resultStatsDiv.asText()); // About 309,000 results
+        webClient.close();
     }
 }
